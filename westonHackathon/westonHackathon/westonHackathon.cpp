7600c09cd0b7c6ced84f932c03af3a73baf3d168
SargentBash <gabby290>
2016-03-10 11:51:04 +0000

    SHe was a 7 out of 10

--- westonHackathon/westonHackathon/westonHackathon.cpp
+++ westonHackathon/westonHackathon/westonHackathon.cpp
@@ -12,12 +12,19 @@ int main()
 {
 	while (1 < 2) 
 	{
+		system("cls");
 		system("color 0A");
+		system("cls");
 		system("color 2B");
+		system("cls");
 		system("color 3C");
+		system("cls");
 		system("color 4D");
+		system("cls");
 		system("color 5E");
+		system("cls");
 		system("color 6F");
+		system("cls");
 
 		//code here, well done Dan I'm glad you know where code needs to go.
 
