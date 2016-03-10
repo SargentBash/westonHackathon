0dbe5f686dee9aaddccebcce9224a46a10f4271a
SargentBash <gabby290>
2016-03-10 11:35:21 +0000

    Bacon Bagget no Ketchup

--- westonHackathon/westonHackathon/westonHackathon.cpp
+++ westonHackathon/westonHackathon/westonHackathon.cpp
@@ -9,7 +9,18 @@ using namespace std;
 
 int main()
 {
-	while (1 < 2) {
+	while (1 < 2) 
+	{
+		system("colour 0A");
+		system("colour 0B");
+		system("colour 0C");
+		system("colour 0D");
+		system("colour 0E");
+		system("colour 0F");
+		system("colour 0G");
+		system("colour 0H");
+		system("colour 0I");
+		system("colour 0J");
 		//code here, well done Dan I'm glad you know where code needs to go.
 
 
